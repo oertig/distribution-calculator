25b9145f669403f403d1eb40233280e2a6cfb32f
oertig <147935788+oertig@users.noreply.github.com>
2025-01-21 18:51:43 +0100

    remove superfluous statement

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-use DistributionCalculator;
-
 include_once('ProbabilityDistribution.class.php');
 $probabilityDistribution = new DistributionCalculator\ProbabilityDistribution();
 
